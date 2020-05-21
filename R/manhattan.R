3a5b52ff5a22f1a6c984f8073e0bcf32fdacdb55
Bioinformatic Research Group in Epidemiology (BRGE) <juanr.gonzalez@isglobal.org>
2020-05-21 17:37:51 +0200

    added manhattan plot

---
+++ R/manhattan.R
@@ -0,0 +1,93 @@
+#' @title Mannhattan Plot
+#' 
+#' @description Performs Mannhattan plot for GWAS, DGE or EWAS analysis
+#'
+#' @param x an object of class dsGWAS 
+#' @param featureCol column having feature name (e.g. SNP, CpG, gene, ..)
+#' @param chrCol column having chromosome name
+#' @param positionCol column having basepair position
+#' @param sig genome-wide significant level. Default 5x10-8
+#' @param sugg suggestive significant level. Default 1x10-6
+#' @param threshold numeric value or NA for no label; label snps with pvalue < threshold. Default is NA
+#' @param hlight list of features names to highlight. For no SNP higlighting, use NA (default)
+#' @param col vector listing color/colors desired for manhattan plot. Default c("#E2709A", "#CB4577", "#BD215B", "#970F42", "#75002B")
+#' @param ylims y limits to be visualized in -log10 scale. Default is c(1.5,9)
+#' @param title plot title, with quotations (e.g. "My Manhattan Plot")
+#' @param ... additional ggplot arguments (ylim, cex, etc. can be altered within the plot section of the function)
+#'
+#' @return a ggplot object having the Manhattan plot
+#' 
+#' @author modified from here: https://github.com/pcgoddard/Burchardlab_Tutorials/wiki/GGplot2-Manhattan-Plot-Function
+#'
+#' @export
+#' @import ggrepel
+#' @import RColorBrewel 
+
+
+manhattan <- function(x, featureCol, chrCol, posCol, pvalCol, sig= 5e-8,
+                      sugg = 1e-6, threshold=NA, hlight=NA, 
+                      col=c("#E2709A", "#CB4577", "#BD215B", "#970F42", "#75002B"), 
+                      ylims = c(1.5,9), title="Manhattan plot", ...){
+  
+  df <- x[ , c(featureCol, chrCol, posCol, pvalCol)]
+  names(df) <- c("SNP", "CHR", "BP", "P")
+  df <- dplyr::filter(df, CHR%in%c(1:22))
+  df$BP <- as.numeric(df$BP)
+  df$CHR <- as.numeric(df$CHR)
+
+  df.tmp <- df %>% 
+      # Compute chromosome size
+      group_by(CHR) %>% 
+      summarise(chr_len=max(BP)) %>% 
+      
+      # Calculate cumulative position of each chromosome
+      mutate(tot=cumsum(chr_len)-chr_len) %>%
+      select(-chr_len) %>%
+      
+      # Add this info to the initial dataset
+      left_join(df, ., by=c("CHR"="CHR")) %>%
+      
+      # Add a cumulative position of each SNP
+      arrange(CHR, BP) %>%
+      mutate( BPcum=BP+tot) %>%
+      
+      # Add highlight and annotation information
+      mutate( is_highlight=ifelse(SNP %in% hlight, "yes", "no")) %>%
+      mutate( is_annotate=ifelse(P < threshold, "yes", "no"))
+    
+    # get chromosome center positions for x-axis
+    axisdf <- df.tmp %>% group_by(CHR) %>% summarize(center=( max(BPcum) + min(BPcum) ) / 2 )
+    
+    ggplot(df.tmp, aes(x=BPcum, y=-log10(P))) +
+      # Show all points
+      geom_point(aes(color=as.factor(CHR)), alpha=0.8, size=2) +
+      scale_color_manual(values = rep(col, 22 )) +
+      
+      # custom X axis:
+      scale_x_continuous( label = axisdf$CHR, breaks= axisdf$center ) +
+      scale_y_continuous(expand = c(0, 0), limits = ylims) + # expand=c(0,0)removes space between plot area and x axis 
+      
+      # add plot and axis titles
+      ggtitle(paste0(title)) +
+      labs(x = "Chromosome") +
+      
+      # add genome-wide sig and sugg lines
+      geom_hline(yintercept = -log10(sig)) +
+      geom_hline(yintercept = -log10(sugg), linetype="dashed") +
+      
+      # Add highlighted points
+      #geom_point(data=subset(df.tmp, is_highlight=="yes"), color="orange", size=2) +
+      
+      # Add label using ggrepel to avoid overlapping
+      geom_label_repel(data=df.tmp[df.tmp$is_annotate=="yes",], aes(label=as.factor(SNP), alpha=0.7), size=5, force=1.3) +
+      
+      # Custom the theme:
+      theme_bw(base_size = 22) +
+      theme( 
+        plot.title = element_text(hjust = 0.5),
+        legend.position="none",
+        panel.border = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank()
+      ) 
+    }
---
+++ R/qqplot.R
@@ -0,0 +1,49 @@
+#' Create a quantile-quantile plot with ggplot2.
+#'
+#' Assumptions:
+#'   - Expected P values are uniformly distributed.
+#'   - Confidence intervals assume independence between tests.
+#'     We expect deviations past the confidence intervals if the tests are
+#'     not independent.
+#'     For example, in a genome-wide association study, the genotype at any
+#'     position is correlated to nearby positions. Tests of nearby genotypes
+#'     will result in similar test statistics.
+#'
+#' @param ps Vector of p-values.
+#' @param ci Size of the confidence interval, 95% by default.
+#' @return A ggplot2 plot.
+#' @examples
+#' library(ggplot2)
+#' gg_qqplot(runif(1e2)) + theme_grey(base_size = 24)
+
+qqplot <- function(ps, ci = 0.95) {
+  
+  inflation <- function(x) {
+    chisq <- qchisq(1 - x, 1)
+    lambda <- median(chisq) / qchisq(0.5, 1)
+    lambda
+  }
+  
+  n  <- length(ps)
+  df <- data.frame(
+    observed = -log10(sort(ps)),
+    expected = -log10(ppoints(n)),
+    clower   = -log10(qbeta(p = (1 - ci) / 2, shape1 = 1:n, shape2 = n:1)),
+    cupper   = -log10(qbeta(p = (1 + ci) / 2, shape1 = 1:n, shape2 = n:1))
+  )
+  log10Pe <- expression(paste("Expected -log"[10], plain((p.value))))
+  log10Po <- expression(paste("Observed -log"[10], plain((p.value))))
+  ggplot(df) +
+    geom_ribbon(
+      mapping = aes(x = expected, ymin = clower, ymax = cupper),
+      alpha = 0.1
+    ) +
+    geom_point(aes(expected, observed), size = 1.2) +
+    geom_abline(intercept = 0, slope = 1, alpha = 0.5) +
+    # geom_line(aes(expected, cupper), linetype = 2, size = 0.5) +
+    # geom_line(aes(expected, clower), linetype = 2, size = 0.5) +
+    xlab(log10Pe) + ylab(log10Po) + 
+    annotate(geom = "text", x = min(expected), y = max(observed), hjust = 1, 
+             label = sprintf("λ = %.2f", inflation(x$Score.pval)),
+             size = 4)
+}
