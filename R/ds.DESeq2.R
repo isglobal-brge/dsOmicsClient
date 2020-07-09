30f88ffeb43eedd3fdb20791da7e78939417d833
Leire Abarrategui Martinez <nla94@newcastle.ac.uk>
2020-07-09 08:28:17 +0100

    DESeq2 function

---
+++ R/ds.DESeq2.R
@@ -0,0 +1,71 @@
+#'
+#' @title Server-side Differential gene expression analysis using DESeq2
+#' @description This function is similar to Bioconductor's
+#' \code{DESeq2} package \code{DESeq} function. 
+#' @details Implementation of Bioconductor's \code{DESeq2} in DataSHIELD 
+#' infraestructure. 
+#' @param model formula indicating the condition and other covariates to be ajusted
+#' @param set a \code{RangedSummarizedExperiment} 
+#' @param test \code{"Wald"} or \code{"LRT"}
+#' @param fitType \code{"parametric"}, \code{"local"}, or \code{"mean"}
+#' @param sfType 	\code{"ratio"}, \code{"poscounts"}, or \code{"iterate"}
+#' @param betaPrior TRUE or FALSE
+#' @param reduced a reduced formula for \code{test="LRT"}
+#' @param contrast the comparison to extract from the object to build the results table 
+#' @param datasources a list of \code{\link{DSConnection-class}} objects obtained after login. 
+#' If the \code{datasources} argument is not specified
+#' the default set of connections will be used: see \code{\link{datashield.connections_default}}.
+#' @return Data frame containing the results of the Differential Gene Expression analysis 
+#' @author L. Abarrategui, for DataSHIELD development team
+#' @export
+#'
+
+ds.DESeq2 <- function(model, set, test = "Wald",
+                    fitType = "parametric", sfType = "ratio",
+                    betaPrior = FALSE, reduced = NULL, 
+                    contrast = NULL, datasources=NULL)
+{   
+    if (is.null(datasources))
+    {
+        datasources <-  DSI::datashield.connections_find()
+    }
+    
+    if(is.null(model)){
+      stop(" Please provide a valid model formula", call.=FALSE)
+    }
+    
+    vars <- all.vars(model)
+    vars<- paste(vars, collapse=",")
+    
+    if(test != "Wald" & test != "LRT")
+      {
+      stop("Function argument 'test' has to be either 'Wald' or  'LRT'", call.=FALSE)
+    }   
+    
+    if(fitType != "parametric" & fitType != "local" & fitType != "mean")
+    {
+      stop("Function argument 'fitType' has to be either 'parametric','local' or 'mean'", call.=FALSE)
+    }  
+    
+    if(sfType != "ratio" & sfType != "poscounts" & sfType != "iterate")
+    {
+      stop("Function argument 'sfType' has to be either 'ratio', 'poscounts', 'iterate'", call.=FALSE)
+    }  
+    
+    if(!is.null(reduced))
+      {
+       reduced<-attributes(stats::terms(stats::as.formula(reduced)))$intercept
+       reduced<-as.character(reduced)
+      }
+      
+    
+    if(!is.null(contrast)){
+      contrast <- paste(contrast, collapse=",")
+    }
+    # call the server side function
+    calltext <- call("DESeq2DS",vars, set, test, fitType, sfType, betaPrior, reduced, contrast)
+    output <- datashield.aggregate(datasources, calltext)
+    return(output)
+}
+#ds.DESeq2
+#
