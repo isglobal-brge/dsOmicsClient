ac5f0bdc1f4a01308adaaa933dfff75785dc29d6
Xavier Escribà Montagut <xescriba@isglobal.lan>
2021-05-03 16:51:32 +0200

    Update ds.getSNPSbyGen.R

--- R/ds.getSNPSbyGen.R
+++ R/ds.getSNPSbyGen.R
@@ -24,9 +24,4 @@ ds.getSNPSbyGen <- function(gdsFile, genes, name = NULL, datasources = NULL){
   cally <- paste0("getSNPSbyGenDS(", gdsFile, ", old_assign = FALSE, '", paste0(genes, collapse = "','"), "')")
   DSI::datashield.assign.expr(datasources, name, as.symbol(cally))
   
-  if(name != gdsFile){
-    cally <- paste0("getSNPSbyGenDS(", gdsFile, ", old_assign = TRUE)")
-    DSI::datashield.assign.expr(datasources, gdsFile, as.symbol(cally))
-  }
-  
 }
