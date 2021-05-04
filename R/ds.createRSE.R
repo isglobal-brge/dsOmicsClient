85be778a5c45e1c9eed6225be167cee95249bc85
Xavier Escribà Montagut <xescriba@isglobal.lan>
2021-05-04 17:12:09 +0200

    createRSE, annotationcols upgrade

--- R/ds.createRSE.R
+++ R/ds.createRSE.R
@@ -3,8 +3,8 @@
 #' 
 #' @details The RNAseq count table has to have the following structure: \cr
 #' - First column: Entrez identificator \cr
-#' - Second column: Annotated symbol \cr
-#' - All the remaining columns: One col. per individual \cr
+#' - All the remaining columns: One col. per individual + annotation columns (no special
+#' order required, they can even be mixed) \cr
 #' The phenotypes table has to have the following structure: \cr
 #' - First column: ID of the individuals. To match with the column names 
 #' of the RNAseq table \cr
@@ -15,12 +15,15 @@
 #' @param newobj.name \code{character} (default \code{NULL}) Name to be assigned 
 #' on the server to the RangedSummarizedExperiment. If \code{NULL} the created RSE will 
 #' be assigned to a variable named \code{'RSE'}
+#' @param annotCols \code{character vector} (default \code{NULL}) Columns of the counts table to be used as 
+#' annotation when creating the SummarizedExperiment. If \code{NULL}, biomaRt will be used to find 
+#' annotation using the first column of the counts table (EntrezID)
 #' @param datasources a list of \code{\link{DSConnection-class}} (default \code{NULL}) objects obtained after login
 #'
 #' @return This function does not have an output. It creates (or overwrites) a data frame on the study server.
 #' @export
 
-ds.createRSE <- function(rnaseq, phe, newobj.name = NULL, datasources = NULL){
+ds.createRSE <- function(rnaseq, phe, annotCols = NULL, newobj.name = NULL, datasources = NULL){
   
   if (is.null(datasources)) {
     datasources <- DSI::datashield.connections_find()
@@ -40,8 +43,8 @@ ds.createRSE <- function(rnaseq, phe, newobj.name = NULL, datasources = NULL){
   if(!any((cls2 %in% c("data.frame")))){
     stop("[",phe,"] is not a 'data.frame'")
   }
-  
-  cally <- paste0("createRSEDS(", rnaseq, ", ", phe, ")")
+  cally <- paste0("createRSEDS(", rnaseq, ", ", phe, ", ", 
+                  if(is.null(annotCols)){"NULL"}else{paste0("'",paste(annotCols, collapse = "','"),"'")}, ")")
   DSI::datashield.assign.expr(datasources, newobj.name, cally)
   
 }
