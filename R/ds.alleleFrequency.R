53e47b1cadaf0911a93a42412c1cf9f4074b4dc6
Xavier Escribà Montagut <xescriba@isglobal.lan>
2021-05-04 17:11:14 +0200

    allelefreq, no need for sex column identification

--- R/ds.alleleFrequency.R
+++ R/ds.alleleFrequency.R
@@ -9,12 +9,6 @@
 #'  but not for sex chromosomes.
 #'
 #' @param genoData \code{character} Name of the \code{\link{GenotypeData}} object on the server
-#' @param sexcol \code{character} (default \code{"sex"}) Name of the sex column on the covariates file used to create the 
-#' \code{\link{GenotypeData}} object
-#' @param male \code{character} (default \code{"M"}) Name of the male indicator of the sex column on the covariates file used to create the 
-#' \code{\link{GenotypeData}} object. (Note that it is case sensitive so it's not the same \code{male} than \code{Male})
-#' @param female \code{character} (default \code{"M"}) Name of the female indicator of the sex column on the covariates file used to create the 
-#' \code{\link{GenotypeData}} object. (Note that it is case sensitive so it's not the same \code{female} than \code{Female})
 #' @param datasources a list of \code{\link{DSConnection-class}} objects obtained after login. 
 #'
 #' @return A \code{data frame} with a row for each SNP. Columns "M" for males, "F" for females, and "all" 
@@ -22,13 +16,13 @@
 #' "n.M", "n.F", and "n", respectively. "MAF" is the minor allele frequency over all scans.
 #' @export
 
-ds.alleleFrequency <- function(genoData, sexcol = "sex", male = "M", female = "F", datasources = NULL){
+ds.alleleFrequency <- function(genoData, datasources = NULL){
   
   if (is.null(datasources)) {
     datasources <- DSI::datashield.connections_find()
   }
   
-  cally <- paste0("alleleFrequencyDS(", genoData, ", '", sexcol, "', '", male, "', '", female, "')")
+  cally <- paste0("alleleFrequencyDS(", genoData, ")")
   alFreq <- DSI::datashield.aggregate(datasources, as.symbol(cally))
   
   class(alFreq) <- c("dsalleleFrequency", class(alFreq))
