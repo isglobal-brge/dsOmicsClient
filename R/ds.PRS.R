5fd6526bd1ebee5bce22aca374309b058fa0ec5a
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2021-06-07 12:21:03 +0200

    prs documentation update + version bump to match dsOmics dev version

--- R/ds.PRS.R
+++ R/ds.PRS.R
@@ -1,13 +1,31 @@
-#' Title
+#' @title Get Ploygenic Risk Score
+#' @subtitle Using server side VCF resources and retrieving the risk 
+#' factors from the PGS catalog (https://www.pgscatalog.org/)
+#' 
+#' @details This function resolves a list of resources subsetting them by the 
+#' SNPs of risk, this does not ensure that all the SNPs of risk will be found on the 
+#' data. From all the found SNPs of risk, if an individual has less than 'snp_threshold' (percetage)
+#' of SNPs with data, it will be dropped (SNP with no data is marked on the VCF as ./.). If an individual 
+#' passes this threshold filter but still has SNPs with no data, those SNPs will be counted on the 
+#' polygenic risk score as non-risk-alleles, to take this infomation into account, the number of SNPs 
+#' with data for each individual is returned as 'n_snps'.
 #'
-#' @param resources 
-#' @param pgs_id 
-#' @param datasources 
+#' @param resources \code{list} of all the VCF resources with biallelic genotype information. It is advised to 
+#' have one VCF resource per chromosome, a big VCF file with all the information is always slower 
+#' to use.
+#' @param pgs_id \code{character} ID of the PGS catalog to be used to calculate the polygenic risk score. 
+#' Polygenic Score ID & Name from https://www.pgscatalog.org/browse/scores/
+#' @param snp_threshold \code{numeric} (default \code{80}) Threshold to drop individuals. See details for 
+#' further information.
+#' @param datasources a list of \code{\link{DSConnection-class}} (default \code{NULL}) objects obtained after login
 #'
-#' @return
+#' @return \code{data.frame} were the rownames are the individuals. The columns found are: \cr
+#' prs: Polygenic risk score per individual \cr
+#' prs_nw: Polygenic risk score without weights (weight 1 for each risk allele) \cr
+#' n_snps: Number of SNPs with information for each individual
 #' @export
 #'
-#' @examples
+
 ds.PRS <- function(resources, pgs_id, snp_threshold = 80, datasources = NULL){
   
   if (is.null(datasources)) {
@@ -39,13 +57,25 @@ ds.PRS <- function(resources, pgs_id, snp_threshold = 80, datasources = NULL){
   DSI::datashield.aggregate(datasources, as.symbol(cally))
 }
 
-#' Title
+#' @title Internal function: Get PGS catalog table of polygenic risks
 #'
-#' @param pgs_id 
+#' @param pgs_id \code{character} ID of the PGS catalog to be used to calculate the polygenic risk score. 
+#' Polygenic Score ID & Name from https://www.pgscatalog.org/browse/scores/
 #'
-#' @return
+#' @return \code{data.frame} with the columns: \cr
+#' - If chr_name and poition are found: \cr
+#' + start \cr
+#' + end \cr
+#' + effect_allele \cr
+#' + effect_weight \cr
+#' + weight_type (if present on the catalog) \cr
+#' - If rsID is found: \cr
+#' + rsID \cr
+#' + effect_allele \cr
+#' + effect_weight \cr
+#' + weight_type (if present on the catalog) \cr
 #'
-#' @examples
+
 .retrievePGS <- function(pgs_id){
   pgs <- httr::GET(paste0("https://www.pgscatalog.org/rest/score/", pgs_id))
   pgs_text <- httr::content(pgs, "text")
