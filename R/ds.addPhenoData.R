3a4fdb1eafe9f97bec83c597020f186768916b18
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2021-06-10 12:51:12 +0200

    ds.addPhenoData, added option to merge by column of eSet pheno slot

--- R/ds.addPhenoData.R
+++ R/ds.addPhenoData.R
@@ -11,6 +11,9 @@
 #' @param pheno \code{character} Name of the data.frame with the new phenotypes on the 
 #' study server
 #' @param identifier \code{character} (default \code{"ID"}) Name of the ID column on the phenotypes data.frame
+#' @param alternate_eset_id \code{character} (default \code{NULL}) Alternate ID of the eSet pheno data, by default the rownames 
+#' of the eSet pheno data act as ID, use this argument if the ID to merge the individuals is on a column of the pheno data. 
+#' Input NULL for the standard behaviour of using the rownames of the pheno data as ID.
 #' @param newobj.name \code{character} (default \code{NULL}) If \code{NULL}, the original ExpressionSet will be overwritten,
 #' otherwise the new ExpressionSet will be assigned to a variable named after this argument
 #' @param complete_cases \code{bool} (default \code{TRUE}) If \code{TRUE} only the matching individuals 
@@ -21,7 +24,8 @@
 #' @return This function does not have an output. It creates (or overwrites) a data frame on the study server.
 #' @export
 
-ds.addPhenoData <- function(x, pheno, identifier = "ID", newobj.name = NULL, complete_cases = TRUE, datasources = NULL){
+ds.addPhenoData <- function(x, pheno, identifier = "ID", alternate_eset_id = NULL,
+                            newobj.name = NULL, complete_cases = TRUE, datasources = NULL){
   
   if(is.null(datasources)){
     datasources <- datashield.connections_find()
@@ -42,7 +46,9 @@ ds.addPhenoData <- function(x, pheno, identifier = "ID", newobj.name = NULL, com
     stop("The 'pheno' is not a 'data.frame'")
   }
   
-  cally <- paste0("addPhenoDataDS(", x, ", ", pheno, ", '", identifier, "', ", complete_cases, ")")
+  cally <- paste0("addPhenoDataDS(", x, ", ", pheno, ", '", identifier, "', ", 
+                  if(is.null(alternate_eset_id)){"NULL, "}else{paste0("'",alternate_eset_id,"', ")},
+                  complete_cases, ")")
   DSI::datashield.assign.expr(datasources, newobj.name, as.symbol(cally))
   
 }
