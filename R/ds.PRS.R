44acc9c42b29ede1113f78a342e92aee50886440
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2021-09-21 18:05:19 +0200

    prs updated

--- R/ds.PRS.R
+++ R/ds.PRS.R
@@ -10,7 +10,7 @@
 #' polygenic risk score as non-risk-alleles, to take this infomation into account, the number of SNPs 
 #' with data for each individual is returned as 'n_snps'.
 #' 
-#' When using a user provided ROI table instead of a PGS catalog ID to calculate the PRS, it is important to note that 
+#' When using a user provided prs_table table instead of a PGS catalog ID to calculate the PRS, it is important to note that 
 #' the provided data.frame has to have a very strict structure regarding column names (order is not relevant). Please 
 #' follow one of this two schemas: \cr
 #' - Schema 1 (provide SNP positions): \cr
@@ -23,109 +23,136 @@
 #' 
 #' As a rule of thumb, it is advised to use when possible the Schema 1 (provide SNP positions), as the implementation 
 #' to subset the VCF files is miles faster. 
+#' 
+#' Since the actual results of the PRS is sensitive information, the results are not returned to the client, 
+#' however they can be merged into a table on the server(s). The main use of that is to add the PRS results 
+#' to a phenotypes table and assess relationships between PRS scores and the phenotypes. This merge is performed 
+#' via the individuals ID, specified on the argument (\code{table_id_column}); the table is specified using 
+#' the argument \code{table}. When merging the results to a table, by default the column names will be: \cr
+#' - When using \code{pgs_id}: \cr
+#' + prs_\code{pgs_id} \cr
+#' + prs_nw_\code{pgs_id} \cr
+#' - When using \code{prs_table}: \cr
+#' + prs_prs_custom_results \cr
+#' + prs_nw_prs_custom_results \cr
+#' 
+#' If another designation is desired, make use of the \code{table_prs_name} argument, which by default is 
+#' \code{NULL}. Note that this parameter only changes the tail 
+#' of the names, the columns added (2) will begin by \code{prs_} and \code{prs_nw_}. This columns correspond to the 
+#' actual PRS calculated and the PRS without weights (or PRS where all weights equal 1).
 #'
 #' @param resources \code{list} of all the VCF resources with biallelic genotype information. It is advised to 
 #' have one VCF resource per chromosome, a big VCF file with all the information is always slower 
 #' to use.
 #' @param pgs_id \code{character} (default \code{NULL}) ID of the PGS catalog to be used to calculate the polygenic risk score. 
 #' Polygenic Score ID & Name from https://www.pgscatalog.org/browse/scores/
-#' @param ROI \code{data.frame} (default \code{NULL}) Table containing the genomic region of interest to calculate 
-#' the polygenic risk score. See the details for information of the structure this table has to have.
+#' @param prs_table \code{data.frame} (default \code{NULL}) Table containing the genomic region of interest to calculate 
+#' the polygenic risk score. See the details for information of the structure this table has to have.\
+#' @param table \code{character} (default \code{NULL}) If not \code{NULL}, it is the name of the table (on the server(s)) 
+#' that will be used to merge the PRS results (typically a phenotypes table).
+#' @param table_id_column \code{character} (default \code{NULL}) Argument only used when the \code{table} argument 
+#' is supplied, it corresponds to the column name of the \code{table} that contains the individual IDs to perform 
+#' the merge.
+#' @param table_prs_name \code{character} (default \code{NULL}) If not \code{NULL} it's the name that will be 
+#' used to design the column names added to \code{table}. Read the details for further information.
 #' @param snp_threshold \code{numeric} (default \code{80}) Threshold to drop individuals. See details for 
 #' further information.
 #' @param datasources a list of \code{\link{DSConnection-class}} (default \code{NULL}) objects obtained after login
 #'
-#' @return \code{data.frame} were the rownames are the individuals. The columns found are: \cr
-#' prs: Polygenic risk score per individual \cr
-#' prs_nw: Polygenic risk score without weights (weight 1 for each risk allele) \cr
-#' p_prs_nw: Risk probability using prs_nw and SNPassoc::pscore \cr
-#' n_snps: Number of SNPs with information for each individual
+#' @return The function has no client return. The results are stored on the server(s)
 #' @export
 #'
 
-ds.PRS <- function(resources, pgs_id = NULL, ROI = NULL, table = NULL, table_id_column = NULL,
+ds.PRS <- function(resources, pgs_id = NULL, prs_table = NULL, table = NULL, table_id_column = NULL,
                    table_prs_name = NULL, snp_threshold = 80, datasources = NULL){
   
   if (is.null(datasources)) {
     datasources <- DSI::datashield.connections_find()
   }
   
-  # Logic to check if ROI or pgs_id is supplied. If both are supplied, the ROI wil be used.
-  if(!is.null(ROI) & !is.null(pgs_id)){
-    warning('Both [ROI] and [pgs_id] supplied, only the [ROI] will be used to calculate the PRS.')
+  # Logic to check if prs_table or pgs_id is supplied. If both are supplied, the prs_table wil be used.
+  if(!is.null(prs_table) & !is.null(pgs_id)){
+    warning('Both [prs_table] and [pgs_id] supplied, only the [prs_table] will be used to calculate the PRS.')
     pgs_id <- NULL
   }
-  else if(!is.null(ROI)){
-    # Check the structure of the supplied ROI is as required (check details of roxygen), plus remove
+  else if(!is.null(prs_table)){
+    # Check the structure of the supplied prs_table is as required (check details of roxygen), plus remove
     # unneeded columns
-    if(all(c("chr_name", "chr_position", "effect_allele", "reference_allele", "effect_weight") %in% colnames(ROI))){
-      ROI <- ROI %>% dplyr::select(chr_name, chr_position, effect_allele, reference_allele, effect_weight)
-      ROI <- .recodeROI(ROI)
-    } else if (all(c("rsID", "effect_allele", "reference_allele", "effect_weight") %in% colnames(ROI))){
-      ROI <- ROI %>% dplyr::select(rsID, effect_allele, reference_allele, effect_weight)
-      ROI <- .recodeROI(ROI)
+    if(all(c("chr_name", "chr_position", "effect_allele", "reference_allele", "effect_weight") %in% colnames(prs_table))){
+      prs_table <- prs_table %>% dplyr::select(chr_name, chr_position, effect_allele, reference_allele, effect_weight)
+      prs_table <- .recodeprs_table(prs_table)
+    } else if (all(c("rsID", "effect_allele", "reference_allele", "effect_weight") %in% colnames(prs_table))){
+      prs_table <- prs_table %>% dplyr::select(rsID, effect_allele, reference_allele, effect_weight)
+      prs_table <- .recodeprs_table(prs_table)
     } else {
-      stop('The supplied [ROI] table is not structure as required. Please read the @details of ?dsOmicsClient::ds.PRS')
+      stop('The supplied [prs_table] table is not structure as required. Please read the @details of ?dsOmicsClient::ds.PRS')
     }
   }
-  else if(is.null(ROI) & !is.null(pgs_id)){
+  else if(is.null(prs_table) & !is.null(pgs_id)){
     # Get PGS data to later calculate genetic risk score
-    ROI <- .retrievePGS(pgs_id)
+    prs_table <- .retrievePGS(pgs_id)
   }
-  else if(is.null(ROI) & is.null(pgs_id)){
-    error('Supply a [ROI] or [pgs_id] to calculate the PRS.')
+  else if(is.null(prs_table) & is.null(pgs_id)){
+    error('Supply a [prs_table] or [pgs_id] to calculate the PRS.')
   }
   # Assign all the resources to GDSs to be studied later
   assigned_resources <- lapply(names(resources), function(x){
-    if(colnames(ROI) == "rsID"){
+    if(colnames(prs_table) == "rsID"){
       tryCatch({
         DSI::datashield.assign.expr(conns = datasources, symbol = paste0(x, "_gds"), 
-                                    expr = as.symbol(paste0(
+                                    expr = paste0(
                                       "as.resource.object(x = ", resources[[x]],
-                                      ", snps = c('", paste(ROI$rsID, collapse = "', '"), "'))"
-                                    )))
+                                      ", snps = c('", paste(prs_table$rsID, collapse = "', '"), "'))"
+                                    ))
+        paste0(x, "_gds")
       }, error = function(w){
         NULL
       })
     } else {
       tryCatch({
         DSI::datashield.assign.expr(conns = datasources, symbol = paste0(x, "_gds"), 
-                                    expr = as.symbol(paste0(
+                                    expr = paste0(
                                       "as.resource.object(x = ", resources[[x]], ", seq = c('", 
-                                      paste(ROI$chr_name, collapse = "', '"), "'), start.loc = c(",
-                                      paste(ROI$start, collapse = ", "), "), end.loc = c(",
-                                      paste(ROI$end, collapse = ", "), "))"
-                                    )))
+                                      paste(prs_table$chr_name, collapse = "', '"), "'), start.loc = c(",
+                                      paste(prs_table$start, collapse = ", "), "), end.loc = c(",
+                                      paste(prs_table$end, collapse = ", "), "))"
+                                    ))
+        paste0(x, "_gds")
       }, error = function(w){
         NULL
       })
     }
   })
-  # Different cally builds depending on needing to send to the server the ROI with ID or positions
-  if(all(c("chr_name", "start") %in% colnames(ROI))){
-    ROI_type <- "chr_name"
-  } else if ("rsID" %in% colnames(ROI)){
-    ROI_type <- "rsID"
+  # Different cally builds depending on needing to send to the server the prs_table with ID or positions
+  if(all(c("chr_name", "start") %in% colnames(prs_table))){
+    prs_table_type <- "chr_name"
+  } else if ("rsID" %in% colnames(prs_table)){
+    prs_table_type <- "rsID"
   }
 
   # Build cally
   cally <- paste0("PRSDS(c(",
                   paste0(assigned_resources, collapse = ", "),
-                  "), ", snp_threshold, ", '", paste(unlist(ROI), collapse = "', '"),
-                  "', ", if(ROI_type == "rsID"){3}else{5}, ")")
+                  "), ", snp_threshold, ", '", paste(unlist(prs_table), collapse = "', '"),
+                  "', ", if(prs_table_type == "rsID"){3}else{5}, ")")
   DSI::datashield.assign.expr(datasources, "prs_results", cally)
   
   # If table is NULL the results will not be added (join by ID) to a table on the server
   if(!is.null(table)){
     if(is.null(table_id_column)){
-      stop('Please provide the argument "table_id_column" to identify the number of the column that contains the
+      stop('Please provide the argument "table_id_column" to identify the column that contains the
            IDs to merge.')
     }
     if(is.null(table_prs_name)){
-      warning("Default PRS custom name used ['prs_results']")
-      table_prs_name <- 'prs_results'
+      if(!is.null(pgs_id)){
+        warning("Default PRS custom name used ['", pgs_id, "']")
+        table_prs_name <- pgs_id
+      }else{
+        warning("Default PRS custom name used ['prs_custom_results']")
+        table_prs_name <- 'prs_custom_results'
+      }
     }
-    cally <- paste0("PRSDS_aux(prs_results, '", table_prs_name, "', ", table, ", ", table_id_column, ")")
+    cally <- paste0("PRSDS_aux(prs_results, '", table_prs_name, "', ", table, ", '", table_id_column, "')")
     DSI::datashield.assign.expr(datasources, table, cally)
   }
 }
@@ -201,7 +228,7 @@ ds.PRS <- function(resources, pgs_id = NULL, ROI = NULL, table = NULL, table_id_
 
 
 
-.recodeROI <- function(scorings){
+.recodeprs_table <- function(scorings){
   if(c("chr_name", "chr_position") %in% colnames(scorings)){
     data <- data.frame(chr_name = scorings$chr_name,
                        start = scorings$chr_position,
@@ -218,3 +245,4 @@ ds.PRS <- function(resources, pgs_id = NULL, ROI = NULL, table = NULL, table_id_
     return(data)
   }
 }
+
