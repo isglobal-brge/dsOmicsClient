e20245ac283d235f6750eef02113ba30f0a0cd75
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2022-03-24 17:21:49 +0100

    Update plotPCA.R

--- R/plotPCA.R
+++ R/plotPCA.R
@@ -15,12 +15,13 @@ plotPCA <- function(object, group = NULL, xcomp = 1, ycomp = 2, datasources = NU
   if (is.null(datasources)) {
     datasources <- DSI::datashield.connections_find()
   }
-
+  
   data <- dsBaseClient::ds.scatterPlot(x = paste0(object$pca_res, "$Dim.", xcomp), 
                                y = paste0(object$pca_res, "$Dim.", ycomp), 
                                type = "combine",  datasources = datasources, 
                                return.coords = TRUE)
-
+  dev.off()
+  
   if(!is.null(group)){
     cally <- paste0('plotPCADS(', object$pca_res, ', ', object$geno[1],', "', group, '")') # Assumes all geno objects share same phenotypes file
     res <- Reduce(c, datashield.aggregate(datasources, cally))
